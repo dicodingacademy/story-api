3e31def18f6fc280d2f7377ef3cb09cd9ee22187
Dimas Saputra <dimas.maulanads@gmail.com>
2022-01-06 10:10:46 +0700

    create user login aggregate

---
+++ src/Domains/users/aggregate/UserLoginAggregate.ts
@@ -0,0 +1,33 @@
+import { UserRepository } from '../repository';
+import { UserLogin } from '../entities';
+import AuthenticationError from '../../../Commons/exceptions/AuthenticationError';
+import { PasswordHash } from '../security';
+
+class UserLoginAggregate {
+  private userRepository: UserRepository;
+
+  private passwordHash: PasswordHash;
+
+  constructor(userRepository: UserRepository, passwordHash: PasswordHash) {
+    this.userRepository = userRepository;
+    this.passwordHash = passwordHash;
+  }
+
+  async login({ email, password }: UserLogin) {
+    const user = await this.userRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AuthenticationError('User not found');
+    }
+
+    const isPasswordMatch = await this.passwordHash.compare(password, user.hashedPassword);
+
+    if (!isPasswordMatch) {
+      throw new AuthenticationError('Invalid password');
+    }
+
+    return user;
+  }
+}
+
+export default UserLoginAggregate;
---
+++ src/Domains/users/aggregate/_test/UserLoginAggregate.test.ts
@@ -0,0 +1,69 @@
+import { UserRepository } from '../../repository';
+import UserLoginAggregate from '../UserLoginAggregate';
+import AuthenticationError from '../../../../Commons/exceptions/AuthenticationError';
+import { PasswordHash } from '../../security';
+
+describe('UserLoginAggregate', () => {
+  const userRepository = <UserRepository>{};
+  const passwordHash = <PasswordHash>{};
+  const userLoginAggregate = new UserLoginAggregate(userRepository, passwordHash);
+
+  it('should throw error when user not found', async () => {
+    const userLogin = {
+      email: 'dimas@dicoding.com',
+      password: '12345678',
+    };
+    userRepository.findByEmail = jest.fn(() => Promise.resolve(null));
+
+    await expect(userLoginAggregate.login(userLogin))
+      .rejects
+      .toThrowError(AuthenticationError);
+  });
+
+  it('should throw error when password not match', async () => {
+    const userLogin = {
+      email: 'dimas@dicoding.com',
+      password: '12345678',
+    };
+
+    const createdUser = {
+      id: 'user-123',
+      email: 'dimas@dicoding.com',
+      name: 'Dimas',
+      hashedPassword: 'hashedPassword',
+    };
+
+    userRepository.findByEmail = jest.fn(() => Promise.resolve(createdUser));
+    passwordHash.compare = jest.fn(() => Promise.resolve(false));
+
+    await expect(userLoginAggregate.login(userLogin))
+      .rejects
+      .toThrowError(AuthenticationError);
+  });
+
+  it('should return correct registered user', async () => {
+    const userLogin = {
+      email: 'dimas@dicoding.com',
+      password: '12345678',
+    };
+
+    const createdUser = {
+      id: 'user-123',
+      email: 'dimas@dicoding.com',
+      name: 'Dimas',
+      hashedPassword: 'hashedPassword',
+    };
+
+    userRepository.findByEmail = jest.fn(() => Promise.resolve(createdUser));
+    passwordHash.compare = jest.fn(() => Promise.resolve(true));
+
+    const result = await userLoginAggregate.login(userLogin);
+
+    expect(result).toEqual({
+      id: 'user-123',
+      email: 'dimas@dicoding.com',
+      name: 'Dimas',
+      hashedPassword: 'hashedPassword',
+    });
+  });
+});
